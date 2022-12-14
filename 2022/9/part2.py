0829e1d46c0b7fe9b4dfa48448fda33ed51a57bc
Erwan Bancourt <erwan.bancourt@rca.fr>
2022-12-14 16:47:19 +0100

    fix: rename parts day 9

--- 2022/9/part2.py
+++ 2022/9/part2.py
@@ -1,44 +1,61 @@
+class Knot:
+
+    def __init__(self, parent, child, number):
+        self.x = 0
+        self.y = 0
+        self.parent = parent
+        self.child = child
+        self.number = number
+        self.all_positions = {(0, 0)}
+
+    def add_knots(self, n):
+        current_knot = self
+        for i in range(n):
+            new_knot = Knot(current_knot, None, i + 1)
+            current_knot.child = new_knot
+            current_knot = new_knot
+
+
+def change_pos(knot, x_parent, y_parent):
+    if (x_parent - knot.x) > 1:
+        if abs(y_parent - knot.y) > 1:
+            knot.y = y_parent
+        knot.x += 1
+    elif (x_parent - knot.x) < 1:
+        if abs(y_parent - knot.y) > 1:
+            knot.y = y_parent
+        knot.x -= 1
+    if (y_parent - knot.y) > 1:
+        if abs(x_parent - knot.x) > 1:
+            knot.x = x_parent
+        knot.y += 1
+    elif (y_parent - knot.y) < 1:
+        if abs(x_parent - knot.x) > 1:
+            knot.x = x_parent
+        knot.y -= 1
+    knot.all_positions.add((knot.x, knot.y))
+    print('Node ' + knot.number.__str__() + ' position : ' + (knot.x, knot.y).__str__())
+    if knot.child is not None:
+        change_pos(knot.child, knot.x, knot.y)
+
 with open('./input.txt', 'r') as f:
     head_movements = f.read().strip().split('\n')
-    starting_position = (0, 0)
-    positions_visited = {(0, 0)}
-    tail_position = starting_position
-    head_position = starting_position
+    head = Knot(None, None, 'H')
+    # init knots
+    head.add_knots(9)
     for head_movement in head_movements:
         direction = head_movement.split()[0]
         nb_steps = int(head_movement.split()[1])
         for n in range(nb_steps):
             if direction == "U":
-                head_position = (head_position[0], head_position[1] + 1)
-                if (head_position[1] - tail_position[1]) > 1:
-                    tail_position = (tail_position[0], tail_position[1] + 1)
-                    if (head_position[0] - tail_position[0]) == 1:
-                        tail_position = (tail_position[0] + 1, tail_position[1])
-                    elif (head_position[0] - tail_position[0]) == -1:
-                        tail_position = (tail_position[0] - 1, tail_position[1])
+                head.y += 1
             if direction == 'D':
-                head_position = (head_position[0], head_position[1] - 1)
-                if (head_position[1] - tail_position[1]) < -1:
-                    tail_position = (tail_position[0], tail_position[1] - 1)
-                    if (head_position[0] - tail_position[0]) == 1:
-                        tail_position = (tail_position[0] + 1, tail_position[1])
-                    elif (head_position[0] - tail_position[0]) == -1:
-                        tail_position = (tail_position[0] - 1, tail_position[1])
+                head.y -= 1
             if direction == 'L':
-                head_position = (head_position[0] - 1, head_position[1])
-                if (head_position[0] - tail_position[0]) < -1:
-                    tail_position = (tail_position[0] - 1, tail_position[1])
-                    if (head_position[1] - tail_position[1]) == 1:
-                        tail_position = (tail_position[0], tail_position[1] + 1)
-                    elif (head_position[1] - tail_position[1]) == -1:
-                        tail_position = (tail_position[0], tail_position[1] - 1)
+                head.x -= 1
             if direction == 'R':
-                head_position = (head_position[0] + 1, head_position[1])
-                if (head_position[0] - tail_position[0]) > 1:
-                    tail_position = (tail_position[0] + 1, tail_position[1])
-                    if (head_position[1] - tail_position[1]) == 1:
-                        tail_position = (tail_position[0], tail_position[1] + 1)
-                    elif (head_position[1] - tail_position[1]) == -1:
-                        tail_position = (tail_position[0], tail_position[1] - 1)
-            positions_visited.add(tail_position)
-    print(len(positions_visited).__str__())
+                head.x += 1
+            head.all_positions.add((head.x, head.y))
+            change_pos(head.child, head.x, head.y)
+    knot_9 = head.child.child.child.child.child.child.child.child.child
+    print('Node ' + knot_9.number.__str__() + ' has been on ' + len(knot_9.all_positions).__str__() + ' positions.' )
